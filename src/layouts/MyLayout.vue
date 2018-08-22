ed51d55f834ff5cf0f965417a875e0c057a8c8bb
Travis Roach <travis.roach24@gmail.com>
2018-08-21 21:56:22 -0600

    display nav drawer text on mobile only

--- src/layouts/MyLayout.vue
+++ src/layouts/MyLayout.vue
@@ -1,10 +1,7 @@
 <template>
   <q-layout view="lHh Lpr lFf">
     <q-layout-header>
-      <q-toolbar
-        color="blue-9"
-        :inverted="$q.theme === 'ios'"
-      >
+      <q-toolbar color="blue-9" :inverted="$q.theme === 'ios'">
         <q-btn
           flat
           dense
@@ -14,7 +11,6 @@
         >
           <q-icon name="menu" />
         </q-btn>
-
         <router-link id="home-link" to="/">
           <q-toolbar-title>
             Travis Roach
@@ -23,7 +19,6 @@
         </router-link>
       </q-toolbar>
     </q-layout-header>
-
     <q-layout-drawer
       v-model="leftDrawerOpen"
       :content-class="$q.theme === 'mat' ? 'bg-tertiary' : null"
@@ -32,60 +27,99 @@
       :mini-width='80'
       behavior='desktop'
     >
-      <q-list
-        no-border
-        link
-        inset-delimiter
-      >
+      <q-list no-border link inset-delimiter>
         <q-item @click.native="openURL('https://github.com/troach24/')">
           <q-item-side>
-            <small class="text-weight-light text-white">GitHub</small>
-            <br>
-            <q-icon color="white" name="fab fa-github-square" size=40px />
+            <div v-if="$q.platform.is.mobile">
+              <small class="text-weight-light text-white">GitHub</small>
+              <br>
+            </div>
+            <q-icon
+            v-if="$q.platform.is.desktop"
+              class="q-pt-sm q-pb-sm"
+              color="white"
+              name="fab fa-github-square"
+              size=40px
+            />
+            <q-icon
+              v-if="$q.platform.is.mobile"
+              class="q-pt-xs"
+              color="white"
+              name="fab fa-github-square"
+              size=40px
+            />
           </q-item-side>
-          <!-- <q-item-main label="GitHub" sublabel="github.com/troach24" /> -->
         </q-item>
         <q-item @click.native="openURL('http://www.linkedin.com/in/travis-roach/')">
           <q-item-side>
-            <small class="text-weight-light text-white">LinkedIn</small>
-            <br>
-            <q-icon color="white" name="fab fa-linkedin" size=40px />
+            <div v-if="$q.platform.is.mobile">
+              <small class="text-weight-light text-white">LinkedIn</small>
+              <br>
+            </div>
+            <q-icon
+              v-if="$q.platform.is.desktop"
+              class="q-pt-sm q-pb-sm"
+              color="white"
+              name="fab fa-linkedin"
+              size=40px
+            />
+            <q-icon
+              v-if="$q.platform.is.mobile"
+              class="q-pt-xs"
+              color="white"
+              name="fab fa-linkedin"
+              size=40px
+            />
           </q-item-side>
-          <!-- <q-item-main label="LinkedIn" sublabel="linkedin.com/in/travis-roach/" /> -->
         </q-item>
         <q-item @click.native="openURL('https://twitter.com/TravisRoach24')">
           <q-item-side>
-            <small class="text-weight-light text-white">Twitter</small>
-            <br>
-            <q-icon color="white" name="fab fa-twitter-square" size=40px />
+            <div v-if="$q.platform.is.mobile">
+              <small class="text-weight-light text-white">Twitter</small>
+              <br>
+            </div>
+            <q-icon
+              v-if="$q.platform.is.desktop"
+              class="q-pt-sm q-pb-sm"
+              color="white"
+              name="fab fa-twitter-square"
+              size=40px
+            />
+            <q-icon
+              v-if="$q.platform.is.mobile"
+              class="q-pt-xs"
+              color="white"
+              name="fab fa-twitter-square"
+              size=40px
+            />
           </q-item-side>
-          <!-- <q-item-main label="Twitter" sublabel="@TravisRoach24" /> -->
         </q-item>
-      </q-list>
-
-      <q-list
-        no-border
-        link
-        inset-delimiter
-      >
-          <q-item>
-            <q-item-side>
-              <router-link to="/projects">
+        <q-item>
+          <q-item-side>
+            <router-link to="/projects">
+              <div v-if="$q.platform.is.mobile">
                 <small class="text-weight-light text-white">Projects</small>
                 <br>
-                <q-icon
-                  class="q-pt-sm"
-                  color="white"
-                  name="fas fa-rocket"
-                  size=37px
-                />
-              </router-link>
-            </q-item-side>
-          </q-item>
+              </div>
+              <q-icon
+                v-if="$q.platform.is.desktop"
+                class="q-pt-sm q-pb-sm"
+                color="white"
+                name="fas fa-rocket"
+                size=37px
+              />
+              <q-icon
+                v-if="$q.platform.is.mobile"
+                class="q-pt-xs"
+                color="white"
+                name="fas fa-rocket"
+                size=37px
+              />
+            </router-link>
+          </q-item-side>
+        </q-item>
       </q-list>
-
     </q-layout-drawer>
-
     <q-page-container>
       <router-view />
     </q-page-container>
